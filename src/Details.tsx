36bf18c8e48b751bdd9df8bb239ed64e5c003838
Nchekwube1 <unekwenchekwube@gmail.com>
2021-10-11 13:32:28 +0100

    setting graphql response to state with type complete

--- src/Details.tsx
+++ src/Details.tsx
@@ -1,17 +1,34 @@
 import { getBook } from "./Queries/queries"
 import {graphql} from "react-apollo" 
-import { useContext,FC } from "react"
+import { useContext,FC,useState } from "react"
 import ApolloClient from "apollo-boost"
 import { globalContext } from "./Global"
 import{gql} from "apollo-boost"
 import "./scss/details.css"
+interface booktype {
+             name:string
+                id:string
+                genre:string  
+}
+interface detailRes{
+             name:string
+                id:string
+                genre:string  
+        author?:{
+            id:string
+            name:string
+            age:number
+            books: booktype[]
+        }
+    }
 
 const client = new ApolloClient({
   uri:"http://localhost:5000/graphql",
 })
 
-const Details:FC = ( props:any)=> {
+const Details:FC = ( )=> {
     const {select} = useContext(globalContext)
+    const [gres,setGres] = useState<detailRes|null>(null)
     if(select ===""){
         return(
             <h1 className="click">click on a book panel to display details here</h1>
@@ -39,19 +56,26 @@ const Details:FC = ( props:any)=> {
     }
 }
              `
-         }).then(res=>
-            
-            console.log(res.data.book)
+         }).then((res:any) =>
+        { 
+            let ion:detailRes|null = res.data.book
+            setGres(ion)
+               }
             
             )
+      
+        if(gres === null){
+            return(
+                <h1 className="loading">loading details... </h1>
+            )
+        }
+        return (
+            <h1>{gres?.name}</h1>
+        )
+     
 
-          return (
-        <div className="details">
-            <h1>{select}</h1>
-            
-        </div>
-    )
-    }
+   
+}
   
 }
 
